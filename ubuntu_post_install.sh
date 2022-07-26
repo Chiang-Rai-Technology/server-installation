f3f7de1f0dd30e6bda147aa9d24d44d6055baf63
Berlin <99236358+berlin-168bits@users.noreply.github.com>
2022-07-26 11:12:24 +0700

    Update ubuntu_post_install.sh

--- ubuntu_post_install.sh
+++ ubuntu_post_install.sh
@@ -19,7 +19,7 @@ sudo apt-get install -y nginx
 sudo apt-get install -y curl
 
 #install NodeJS
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 
